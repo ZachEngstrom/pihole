bf6aa18a0d27eefca6a12da02935d2ec21974002
Zach Engstrom <zach.engstrom@bluecrossmn.com>
2024-02-05 16:43:16 -0600

    made a function for the timestamp instead of declaring it before each command

--- pihole_update.sh
+++ pihole_update.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-datetime=$(date +"%Y-%m-%d %T")
-printf "\n\n[%s] INFO: pihole_update.sh initiated\n\n" "${datetime}"
+datetime() {
+  date +"%Y-%m-%d %T"
+}
 
-datetime=$(date +"%Y-%m-%d %T")
-printf "[%s] INFO: running 'sudo apt update'\n" "${datetime}"
+printf "\n\n[%s] INFO: pihole_update.sh initiated\n\n" "$(datetime)"
+
+printf "[%s] INFO: running 'sudo apt update'\n" "$(datetime)"
 sudo apt update
 
-datetime=$(date +"%Y-%m-%d %T")
-printf "\n[%s] INFO: running 'sudo apt list --upgradeable'\n" "${datetime}"
+printf "\n[%s] INFO: running 'sudo apt list --upgradeable'\n" "$(datetime)"
 sudo apt list --upgradeable
 
-datetime=$(date +"%Y-%m-%d %T")
-printf "\n[%s] INFO: running 'sudo apt upgrade -y'\n" "${datetime}"
+printf "\n[%s] INFO: running 'sudo apt upgrade -y'\n" "$(datetime)"
 sudo apt upgrade -y
 
-datetime=$(date +"%Y-%m-%d %T")
-printf "\n[%s] INFO: running 'sudo apt autoremove'\n" "${datetime}"
+printf "\n[%s] INFO: running 'sudo apt autoremove -y'\n" "$(datetime)"
 sudo apt autoremove
 
-datetime=$(date +"%Y-%m-%d %T")
-printf "\n[%s] INFO: running 'sudo pihole -up'\n\n" "${datetime}"
+printf "\n[%s] INFO: running 'sudo pihole -up'\n\n" "$(datetime)"
 sudo pihole -up
 
-datetime=$(date +"%Y-%m-%d %T")
-printf "\n[%s] INFO: running 'sudo pihole -g'\n\n" "${datetime}"
+printf "\n[%s] INFO: running 'sudo pihole -g'\n\n" "$(datetime)"
 sudo pihole -g
 
-datetime=$(date +"%Y-%m-%d %T")
-printf "\n[%s] INFO: running 'sudo reboot'\n\n" "${datetime}"
-printf "[%s] INFO: pihole_update.sh finished\n\n" "${datetime}"
+printf "\n[%s] INFO: pihole update finished\n\n" "$(datetime)"
+printf "[%s] INFO: running 'sudo reboot'\n\n" "$(datetime)"
 printf "********************************************************************************\n\n"
 sudo reboot
